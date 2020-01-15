ce41a6cfc5403821e84ef0d7e8c17a6a9375f198
zi-m <zi.m.github.com@gmail.com>
2020-01-15 22:34:30 +0100

    [gsl] fix-cmake.patch (fix debug build x64-windows-static)

--- ports/ms-gsl/portfile.cmake
+++ ports/ms-gsl/portfile.cmake
@@ -7,9 +7,22 @@ vcpkg_from_github(
     REF 1212beae777dba02c230ece8c0c0ec12790047ea
     SHA512 754d0adf32cea1da759be9adb8a64c301ae1cb8556853411bcea4c400079e8e310f1fb8d03f1f26f81553eab24b75fea24a67b9b51d8d92bb4f266e155938230
     HEAD_REF master
+    PATCHES
+        fix-cmake.patch
 )
 
-file(INSTALL ${SOURCE_PATH}/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DGSL_TEST=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/ms-gsl)
+
+# Clean
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/ms-gsl RENAME copyright)
