b0e9570dcdc2a020a3cd64f15233ae101801c4c4
zi-m <zi.m.github.com@gmail.com>
2020-11-21 18:53:09 +0100

    add-wxwidgets-config

--- ports/wxwidgets/portfile.cmake
+++ ports/wxwidgets/portfile.cmake
@@ -9,6 +9,7 @@ vcpkg_from_github(
     PATCHES
         disable-platform-lib-dir.patch
         fix-stl-build-vs2019-16.6.patch
+        add-wxwidgets-config.patch
 )
 
 set(OPTIONS)
@@ -41,50 +42,10 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 vcpkg_copy_tools(TOOL_NAMES wxrc AUTO_CLEAN)
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-
-file(GLOB DLLS "${CURRENT_PACKAGES_DIR}/lib/*.dll")
-if(DLLS)
-    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin)
-    foreach(DLL ${DLLS})
-        get_filename_component(N "${DLL}" NAME)
-        file(RENAME ${DLL} ${CURRENT_PACKAGES_DIR}/bin/${N})
-    endforeach()
-endif()
-file(GLOB DLLS "${CURRENT_PACKAGES_DIR}/debug/lib/*.dll")
-if(DLLS)
-    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/bin)
-    foreach(DLL ${DLLS})
-        get_filename_component(N "${DLL}" NAME)
-        file(RENAME ${DLL} ${CURRENT_PACKAGES_DIR}/debug/bin/${N})
-    endforeach()
-endif()
-
-# do the copy pdbs now after the dlls got moved to the expected /bin folder above
 vcpkg_copy_pdbs()
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/wxwidgets)
 
-if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/mswu/wx/setup.h)
-    file(RENAME ${CURRENT_PACKAGES_DIR}/lib/mswu/wx/setup.h ${CURRENT_PACKAGES_DIR}/include/wx/setup.h)
-endif()
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/mswu)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/mswud)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/msvc)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 file(INSTALL ${SOURCE_PATH}/docs/licence.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
-vcpkg_from_github(
-    OUT_SOURCE_PATH VCPKG_WX_FIND_SOURCE_PATH
-    REPO CaeruleusAqua/vcpkg-wx-find
-    REF 17993e942f677799b488a06ca659a8e46ff272c9
-    SHA512 0fe07d3669f115c9b6a761abd7743f87e67f24d1eae3f3abee4715fa4d6b76af0d1ea3a4bd82dbdbed430ae50295e1722615ce0ee7d46182125f5048185ee153
-    HEAD_REF master
-)
-
-file(COPY ${VCPKG_WX_FIND_SOURCE_PATH}/FindwxWidgets.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/wxwidgets)
-configure_file(${VCPKG_WX_FIND_SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/wxwidgets/FindwxWidgets.cmake-LICENSE COPYONLY)
-file(COPY ${VCPKG_WX_FIND_SOURCE_PATH}/FindwxWidgets.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/wxwidgets)
-file(COPY ${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/wxwidgets)
-file(COPY ${CMAKE_ROOT}/Modules/FindPackageMessage.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/wxwidgets)
